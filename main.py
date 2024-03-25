d2305f8384e98f6517cd868ff1d1686154813e4b
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-03-25 20:11:02 +0530

    testing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import threading
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from serial import Serial
-
 
 app = FastAPI(
     title="Sensor Monitoring",
@@ -11,6 +12,7 @@ app = FastAPI(
 
 sensor_data_list = []
 lock = threading.Lock()
+templates = Jinja2Templates(directory="templates")
 
 
 def serialmonitor(comport, baudrate):
@@ -29,6 +31,11 @@ async def read_root():
     return "Goto this link http://127.0.0.1:8000/serialmonitor to start monitoring"
 
 
+@app.get("/html", response_class=HTMLResponse)
+async def render_html(request: Request):
+    return templates.TemplateResponse(request=request, name="charts.html", context={"id": sensor_data_list})
+
+
 @app.get("/sensordata")
 async def sensor_data():
     print(sensor_data_list)
@@ -38,7 +45,7 @@ async def sensor_data():
 @app.get("/serialmonitor")
 async def start_monitoring():
     thread = threading.Thread(
-        target=serialmonitor, args=("/dev/ttyACM0", 9600), daemon=True
+        target=serialmonitor, args=("/dev/ttyUSB0", 9600), daemon=True
     )
     thread.start()
     return "Monitoring started Checkout your console and if you want data go to http://127.0.0.1:8000/sensordata"
